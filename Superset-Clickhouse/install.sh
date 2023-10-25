e62f41779c6395af064f46e178ce4a11302fb28f
WARAGON <63374544+wp1631@users.noreply.github.com>
2023-10-25 15:38:07 +0700

    Add distinct filename

--- Superset-Clickhouse/install.sh
+++ Superset-Clickhouse/install.sh
@@ -13,13 +13,13 @@ touch ./docker/requirements-local.txt
 echo "clickhouse-connect>=0.6.8" >> ./docker/requirements-local.txt
 
 # Get predefined compose file with clickhouse definition
-wget https://raw.githubusercontent.com/SiliconHealth/metabase-superset/main/Superset-Clickhouse/docker-compose.yml
+wget https://raw.githubusercontent.com/SiliconHealth/metabase-superset/main/Superset-Clickhouse/superset-clickhouse-docker-compose.yml 
 
 # Build the docker image
-docker compose build --force-rm
+docker compose build --force-rm -f superset-clickhouse-docker-compose.yml
 
 # Create predefined network for superset-clickhouse, if not existed
 docker network create superset-clickhouse
 
 # Run the docker image
-docker compose up -d
+docker compose up -d -f superset-clickhouse-docker-compose.yml

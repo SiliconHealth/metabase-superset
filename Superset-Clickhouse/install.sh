8c856b6c114229e0e3ca320e45a31c003243be4f
WARAGON <63374544+wp1631@users.noreply.github.com>
2023-10-25 15:33:44 +0700

    Add installation script

---
+++ Superset-Clickhouse/install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#  Download Superset initial docker definition and source code
+git clone https://github.com/apache/superset.git
+
+# Get into superset folder
+cd superset
+
+# Create a a local requirement file
+touch ./docker/requirements-local.txt
+
+# Add clickhouse driver to the local requirement file
+echo "clickhouse-connect>=0.6.8" >> ./docker/requirements-local.txt
+
+# Get predefined compose file with clickhouse definition
+wget https://raw.githubusercontent.com/SiliconHealth/metabase-superset/main/Superset-Clickhouse/docker-compose.yml
+
+# Build the docker image
+docker compose build --force-rm
+
+# Create predefined network for superset-clickhouse, if not existed
+docker network create superset-clickhouse
+
+# Run the docker image
+docker compose up -d
